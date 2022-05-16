8a28fbadd38ee26733bf035a0eeeafa2a29e194d
Trung1309 <quoctrung130920@gmail.com>
2022-05-16 22:47:40 +0700

    cap nhat database

--- database.sql
+++ database.sql
@@ -7,3 +7,15 @@ create table NguoiDung(
 	SDT nchar(12) not null,
 	matKhau nchar(20) not null
 )
+
+create table NhanVien(
+	maNV	nvarchar(20) not null primary key,
+	hoTen	nvarchar(50) not null,
+	ngaySinh varchar(50) not null,
+	chucVu	nvarchar(50),
+	email	nvarchar(50),
+	sdt		char(20),
+	gioiTinh nvarchar(10) not null,
+	diaChi	nvarchar(50)
+	
+)
